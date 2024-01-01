3b2afbac1d5148fdff05372bccfb3133ee0ca2fa
Minjae Seon <ddol0225@gmail.com>
2024-01-01 16:53:15 +0900

    chore: change setting

--- src/main/java/dev/aftermoon/jenkins_practice_spring_boot/JenkinsPracticeSpringBootApplication.java
+++ src/main/java/dev/aftermoon/jenkins_practice_spring_boot/JenkinsPracticeSpringBootApplication.java
@@ -2,9 +2,16 @@ package dev.aftermoon.jenkins_practice_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsPracticeSpringBootApplication {
+public class JenkinsPracticeSpringBootApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsPracticeSpringBootApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsPracticeSpringBootApplication.class, args);
