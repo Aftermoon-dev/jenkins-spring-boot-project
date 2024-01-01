885037874cdade0eb14cf6ef5ab9930bea8e356d
Minjae Seon <ddol0225@gmail.com>
2024-01-01 15:28:42 +0900

    Initial Project

---
+++ src/main/java/dev/aftermoon/jenkins_practice_spring_boot/JenkinsPracticeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.aftermoon.jenkins_practice_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPracticeSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPracticeSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aftermoon/jenkins_practice_spring_boot/JenkinsPracticeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aftermoon.jenkins_practice_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPracticeSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
