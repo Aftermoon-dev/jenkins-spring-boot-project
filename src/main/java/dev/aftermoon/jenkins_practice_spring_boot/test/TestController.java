7e1043712610081bd5236a0b7d9ad03d0f267628
Minjae Seon <ddol0225@gmail.com>
2024-01-01 16:59:18 +0900

    feat: create test controller

---
+++ src/main/java/dev/aftermoon/jenkins_practice_spring_boot/test/TestController.java
@@ -0,0 +1,12 @@
+package dev.aftermoon.jenkins_practice_spring_boot.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "This is Test";
+    }
+}
